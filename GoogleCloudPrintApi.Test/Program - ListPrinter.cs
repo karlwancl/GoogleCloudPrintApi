972b15c1aa6231fcd33107cc5957fb9dbe5e6413
Karl Wan <lppkarl@gmail.com>
2016-10-25 23:27:08 +0800

    Tested ListPrinterAsync, UpdatePrinterAsync & DeletePrinterAsync. Updated README.md

---
+++ GoogleCloudPrintApi.Test/Program - ListPrinter.cs
@@ -0,0 +1,32 @@
+﻿using GoogleCloudPrintApi.Models.Printer;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudPrintApi.Test
+{
+    internal partial class Program
+    {
+        static void ListPrinter()
+        {
+            var client = new GoogleCloudPrintClient(provider, token);
+            var request = new ListRequest { Proxy = proxy };
+            var response = client.ListPrinterAsync(request).Result;
+            if (response.Printers != null && response.Printers.Any())
+            {
+                for (int i = 0; i < response.Printers.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {response.Printers.ElementAt(i).DisplayName}");
+                }
+            }
+            else
+                Console.WriteLine("There is no printer in the list");
+        }
+    }
+}
